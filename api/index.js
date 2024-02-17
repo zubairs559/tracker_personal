4e4d5340f2a797f88a135ef9d19460cf3865db74
zubair shaikh <zubairs559@gmail.com>
2024-02-17 11:58:11 +0530

    Cooected to monog db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongo db");
+}).catch((err) => {
+    console.log(err);
+}); 
 
 mongoose.connect
 
