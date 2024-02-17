20ea39d331d2a38fbee51ecf429f6a93161fda4e
zubair shaikh <zubairs559@gmail.com>
2024-02-17 19:06:50 +0530

    Created signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 mongoose.connect
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running');
@@ -20,3 +22,4 @@ app.listen(3000, () => {
 
 
 app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
