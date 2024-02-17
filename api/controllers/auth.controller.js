2bcd3329f7301f7740ed4d86f7ddad023fbde62f
zubair shaikh <zubairs559@gmail.com>
2024-02-17 19:15:35 +0530

    Created Middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHnadler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username , email , password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username , email , password: hashedPassword });
@@ -9,6 +10,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json('User Created Successfullyyyy'); 
   } catch (error) {
-    res.status(500).json(error.message);
+    // next(errorHnadler(550 , 'Error from the functions'));
+    next(error);
   }
 };
